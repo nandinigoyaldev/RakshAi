6a77f954a626c9389cdc2ba63e55d798f105a53e
Nandini <nandunandinigoyal@gmail.com>
2026-06-29 10:29:32 +0530

    kavach

--- test_models.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-import google.generativeai as genai
-
-load_dotenv(os.path.join(os.getcwd(), ".env"))
-genai.configure(api_key=os.environ.get("GEMINI_API_KEY", ""))
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(m.name)
